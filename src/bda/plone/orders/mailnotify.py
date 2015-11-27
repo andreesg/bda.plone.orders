03e88d17960cf67f0f469aacf50daa26f58f4a49
André Gonçalves <andreslb1@gmail.com>
2015-11-27 15:03:52 +0100

    Fix mail notification

--- src/bda/plone/orders/mailnotify.py
+++ src/bda/plone/orders/mailnotify.py
@@ -376,7 +376,7 @@ def create_order_summery(context, order_data):
         _('order_summary_label', default=u'Summary:'), context=request)
     
     summary_text = '<br>'.join([safe_encode(line) for line in lines])
-    return safe_encode(summary_title) + '<br>' summary_text
+    return safe_encode(summary_title) + '<br>' + summary_text
 
 
 def create_global_text(context, order_data):
